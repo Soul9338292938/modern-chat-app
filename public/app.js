59ad4c97b91c0da9804909ca7707db48bcbb3d69
Soul9338292938 <yashvardhanreddypamireddy@gmail.com>
2026-02-06 21:21:08 +0530

    Added frontend UI

---
+++ public/app.js
@@ -0,0 +1,36 @@
+const API = "";
+
+async function signup() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  document.getElementById("msg").innerText =
+    data.success ? "Signup successful! Now login." : data.error;
+}
+
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+
+  if (data.token) {
+    localStorage.setItem("token", data.token);
+    window.location = "/chat.html";
+  } else {
+    document.getElementById("msg").innerText = data.error;
+  }
+}
