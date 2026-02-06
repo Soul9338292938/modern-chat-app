fdb17cf0c50329d6179b774c277168560d8533d0
Soul9338292938 <yashvardhanreddypamireddy@gmail.com>
2026-02-06 21:18:12 +0530

    Enable static frontend

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 /* ================= DATABASE ================= */
 
