f254691ab60c385f6671df030663c4c09271f680
Soul9338292938 <yashvardhanreddypamireddy@gmail.com>
2026-02-06 20:33:08 +0530

    Initial modern chat backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.get("/", (req, res) => {
+  res.send("Modern Chat Backend Running");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("Server running on port " + PORT));
